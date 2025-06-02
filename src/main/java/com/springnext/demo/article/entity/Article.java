2ba7168ef212dd9182558f8e962e8daea8c04ef8
suhyun.p69 <suhyun.p69@gmail.com>
2025-06-02 23:36:53 +0900

    add Article domain

---
+++ src/main/java/com/springnext/demo/article/entity/Article.java
@@ -0,0 +1,10 @@
+package com.springnext.demo.article.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Article {
+    private Long id;
+}
