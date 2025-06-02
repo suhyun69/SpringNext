e0e57653975690d2eeb5218054059bbae9501f43
suhyun.p69 <suhyun.p69@gmail.com>
2025-06-03 01:00:49 +0900

    @Operation 수정

--- src/main/java/com/springnext/demo/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/springnext/demo/article/controller/ApiV1ArticleController.java
@@ -4,6 +4,8 @@ import com.springnext.demo.article.controller.request.ArticleRequest;
 import com.springnext.demo.article.entity.Article;
 import com.springnext.demo.article.service.ArticleService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,23 +24,32 @@ public class ApiV1ArticleController {
     private final ArticleService articleService;
 
     @PostMapping("/article")
-    @ResponseStatus(code = HttpStatus.CREATED)
-    @Operation(summary = "Article 작성", description = "Article을 작성합니다.")
+    @Operation(
+            summary = "Article 작성",
+            description = "Article을 작성합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Article 생성 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+                    @ApiResponse(responseCode = "500", description = "서버 오류")
+            }
+    )
     public ResponseEntity<Article> createArticle(@RequestBody ArticleRequest request) {
         Article savedArticle = articleService.save(request);
 
-        return ResponseEntity.status(HttpStatus.CREATED)
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
                 .body(savedArticle);
     }
 
     @GetMapping("")
-    @ResponseStatus(code = HttpStatus.OK)
+    // @ResponseStatus(code = HttpStatus.OK) // ResponseEntitiy.ok()와 중복됨
     @Operation(summary = "Article 전체 조회", description = "전체 Article을 조회합니다.")
     public ResponseEntity<List<Article>> getArticles() {
 
         List<Article> articles = articleService.findAll();
 
-        return ResponseEntity.ok()
+        return ResponseEntity
+                .ok()
                 .body(articles);
     }
 
@@ -46,7 +57,8 @@ public class ApiV1ArticleController {
     public ResponseEntity<Article> getArticle(@PathVariable Long id) {
         Article article = articleService.findById(id);
 
-        return ResponseEntity.ok()
+        return ResponseEntity
+                .ok()
                 .body(article);
     }
 }
